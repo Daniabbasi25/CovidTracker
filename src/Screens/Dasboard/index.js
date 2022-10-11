1fdc924bb2f473b475be7b4f75480dbf429cb8e2
Danyal Muneer <abbasidani82@gmail.com>
2022-10-11 16:07:51 +0500

    setting up the chart design

--- src/Screens/Dasboard/index.js
+++ src/Screens/Dasboard/index.js
@@ -5,25 +5,78 @@ import {
   TouchableOpacity,
   ScrollView,
   Dimensions,
+  ActivityIndicator,
 } from 'react-native';
+import {LineChart} from 'react-native-chart-kit';
 import React, {useEffect, useState} from 'react';
 import Icon from 'react-native-vector-icons/dist/FontAwesome';
-import {
-  LineChart,
-  BarChart,
-  PieChart,
-  ProgressChart,
-  ContributionGraph,
-  StackedBarChart,
-} from 'react-native-chart-kit';
+
 import {styles} from './styles';
+import {useSelector} from 'react-redux';
 
 const Dashboard = ({route}) => {
-  console.log(route.params);
   const [isTracker, setIsTracker] = useState(true);
   const [dataFilter, setDataFilter] = useState('country');
   const [data, setdata] = useState();
-  useEffect(() => {}, []);
+  const [chartData, setChartData] = useState();
+  const [isloading, setIsloading] = useState(true);
+
+  const country = useSelector(state => state.country.country);
+  useEffect(() => {
+    const getWorldWideHistoryData = async () => {
+      try {
+        const response = await fetch(
+          `https://disease.sh/v3/covid-19/historical/all?lastdays=120`,
+        );
+        const json = await response.json();
+
+        const chartDatafun = buildChartData(json, 'cases');
+        setChartData(chartDatafun);
+        console.log('my data now of 1 country', JSON.stringify(chartData));
+      } catch (error) {
+        console.error(error);
+      } finally {
+        setIsloading(false);
+      }
+    };
+    const getWorldWideData = async () => {
+      try {
+        const response = await fetch(`https://disease.sh/v3/covid-19/all`);
+        const json = await response.json();
+
+        setdata(json);
+
+        console.log('my data now of 1 country', JSON.stringify(data));
+      } catch (error) {
+        console.error(error);
+      } finally {
+        setIsloading(false);
+      }
+    };
+
+    const getcountryData = async () => {
+      try {
+        const response = await fetch(
+          `https://disease.sh/v3/covid-19/countries/${country.value}`,
+        );
+        const json = await response.json();
+
+        setdata(json);
+
+        console.log('my data now of 1 country', JSON.stringify(data));
+      } catch (error) {
+        console.error(error);
+      } finally {
+        setIsloading(false);
+      }
+    };
+    if (dataFilter === 'country') {
+      getcountryData();
+    } else if (dataFilter === 'Worldwide') {
+      getWorldWideData();
+    }
+    getWorldWideHistoryData();
+  }, [country, dataFilter]);
   const myIcon = (
     <Icon
       name="long-arrow-right"
@@ -33,6 +86,22 @@ const Dashboard = ({route}) => {
     />
   );
 
+  const buildChartData = (data, casesTypes = 'cases') => {
+    const chartData = [];
+    let lastdatePoint;
+    for (let date in data.cases) {
+      if (lastdatePoint) {
+        const newDataPoint = {
+          x: date,
+          y: data[casesTypes][data] - lastdatePoint,
+        };
+        chartData.push(newDataPoint);
+      }
+      lastdatePoint = data[casesTypes][data];
+    }
+    return chartData;
+  };
+
   return (
     <View style={{flex: 1, backgroundColor: '#fff'}}>
       {/* <View style={styles.header}>
@@ -41,134 +110,141 @@ const Dashboard = ({route}) => {
           style={styles.covid}
         />
       </View> */}
-      <ScrollView>
-        <View style={styles.headerBox}>
-          <Image
-            source={require('../../../accets/Images/Design.png')}
-            style={styles.doctorImage}
-          />
-          <View style={{marginLeft: '5%'}}>
-            <Text
-              style={{
-                width: '40%',
-                color: '#142237',
-                fontWeight: '500',
-                fontSize: 15,
-                marginRight: '5%',
-                marginBottom: '2%',
-              }}>
-              Know safety tips and precautions from top Doctors.
-            </Text>
-            <TouchableOpacity
-              style={{
-                backgroundColor: '#9156EC',
-                width: '10%',
-                justifyContent: 'center',
-                borderRadius: 20,
-              }}>
-              {myIcon}
+      {isloading ? (
+        <ActivityIndicator />
+      ) : (
+        <ScrollView>
+          <View style={styles.headerBox}>
+            <Image
+              source={require('../../../accets/Images/Design.png')}
+              style={styles.doctorImage}
+            />
+            <View style={{marginLeft: '5%'}}>
+              <Text
+                style={{
+                  width: '40%',
+                  color: '#142237',
+                  fontWeight: '500',
+                  fontSize: 15,
+                  marginRight: '5%',
+                  marginBottom: '2%',
+                }}>
+                Know safety tips and precautions from top Doctors.
+              </Text>
+              <TouchableOpacity
+                style={{
+                  backgroundColor: '#9156EC',
+                  width: '10%',
+                  justifyContent: 'center',
+                  borderRadius: 20,
+                }}>
+                {myIcon}
+              </TouchableOpacity>
+            </View>
+          </View>
+          <View style={styles.buttonBox}>
+            <TouchableOpacity onPress={() => setIsTracker(true)}>
+              <Text
+                style={isTracker ? styles.activeButton : styles.deactiveButton}>
+                Tracker
+              </Text>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => setIsTracker(false)}>
+              <Text
+                style={
+                  !isTracker ? styles.activeButton : styles.deactiveButton
+                }>
+                Symptoms
+              </Text>
             </TouchableOpacity>
           </View>
-        </View>
-        <View style={styles.buttonBox}>
-          <TouchableOpacity onPress={() => setIsTracker(true)}>
-            <Text
-              style={isTracker ? styles.activeButton : styles.deactiveButton}>
-              Tracker
-            </Text>
-          </TouchableOpacity>
-          <TouchableOpacity onPress={() => setIsTracker(false)}>
-            <Text
-              style={!isTracker ? styles.activeButton : styles.deactiveButton}>
-              Symptoms
-            </Text>
-          </TouchableOpacity>
-        </View>
-        <View
-          style={{
-            flexDirection: 'row',
-            justifyContent: 'space-evenly',
-            alignItems: 'center',
-            marginTop: '2%',
-          }}>
-          <TouchableOpacity onPress={() => setDataFilter('country')}>
-            <Text
-              style={[
-                dataFilter === 'country'
-                  ? styles.textactive
-                  : styles.textdeactive,
-                {fontSize: 15},
-              ]}>
-              Country
-            </Text>
-          </TouchableOpacity>
-          <TouchableOpacity onPress={() => setDataFilter('State/Province')}>
-            <Text
-              style={[
-                dataFilter === 'State/Province'
-                  ? styles.textactive
-                  : styles.textdeactive,
-                {fontSize: 15},
-              ]}>
-              State/Province
-            </Text>
-          </TouchableOpacity>
-          <TouchableOpacity onPress={() => setDataFilter('City')}>
-            <Text
-              style={[
-                dataFilter === 'City' ? styles.textactive : styles.textdeactive,
-                {fontSize: 15},
-              ]}>
-              City
-            </Text>
-          </TouchableOpacity>
-          <TouchableOpacity onPress={() => setDataFilter('Worldwide')}>
-            <Text
-              style={[
-                dataFilter === 'Worldwide'
-                  ? styles.textactive
-                  : styles.textdeactive,
-                {fontSize: 15},
-              ]}>
-              Worldwide
-            </Text>
-          </TouchableOpacity>
-        </View>
-        <View style={styles.bigBoxes}>
-          <View style={[styles.boxes, styles.dangrousBox]}>
-            <Text style={[styles.boxHeading, styles.boxHeadingDanger]}>
-              Confirm
-            </Text>
-            <Text style={[styles.numbers, styles.boxHeadingDanger]}>
-              2,37,395
-            </Text>
+          <View
+            style={{
+              flexDirection: 'row',
+              justifyContent: 'space-evenly',
+              alignItems: 'center',
+              marginTop: '2%',
+            }}>
+            <TouchableOpacity onPress={() => setDataFilter('country')}>
+              <Text
+                style={[
+                  dataFilter === 'country'
+                    ? styles.textactive
+                    : styles.textdeactive,
+                  {fontSize: 15},
+                ]}>
+                Country
+              </Text>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => setDataFilter('State/Province')}>
+              <Text
+                style={[
+                  dataFilter === 'State/Province'
+                    ? styles.textactive
+                    : styles.textdeactive,
+                  {fontSize: 15},
+                ]}>
+                State/Province
+              </Text>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => setDataFilter('City')}>
+              <Text
+                style={[
+                  dataFilter === 'City'
+                    ? styles.textactive
+                    : styles.textdeactive,
+                  {fontSize: 15},
+                ]}>
+                City
+              </Text>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => setDataFilter('Worldwide')}>
+              <Text
+                style={[
+                  dataFilter === 'Worldwide'
+                    ? styles.textactive
+                    : styles.textdeactive,
+                  {fontSize: 15},
+                ]}>
+                Worldwide
+              </Text>
+            </TouchableOpacity>
           </View>
-          <View style={[styles.boxes, styles.blueBox]}>
-            <Text style={[styles.boxHeading, styles.boxHeadingBlue]}>
-              Active
-            </Text>
-            <Text style={[styles.numbers, styles.boxHeadingBlue]}>
-              2,37,395
-            </Text>
+          <View style={styles.bigBoxes}>
+            <View style={[styles.boxes, styles.dangrousBox]}>
+              <Text style={[styles.boxHeading, styles.boxHeadingDanger]}>
+                Confirm
+              </Text>
+              <Text style={[styles.numbers, styles.boxHeadingDanger]}>
+                {data.cases}
+              </Text>
+            </View>
+            <View style={[styles.boxes, styles.blueBox]}>
+              <Text style={[styles.boxHeading, styles.boxHeadingBlue]}>
+                Active
+              </Text>
+              <Text style={[styles.numbers, styles.boxHeadingBlue]}>
+                {data.active}
+              </Text>
+            </View>
+            <View style={[styles.boxes, styles.greenBox]}>
+              <Text style={[styles.boxHeading, styles.boxHeadinggreen]}>
+                Recovered
+              </Text>
+              <Text style={[styles.numbers, styles.boxHeadinggreen]}>
+                {data.recovered}
+              </Text>
+            </View>
+            <View style={[styles.boxes, styles.grayBox]}>
+              <Text style={[styles.boxHeading, styles.boxHeadinggray]}>
+                Deaths
+              </Text>
+              <Text style={[styles.numbers, styles.boxHeadinggray]}>
+                {data.deaths}
+              </Text>
+            </View>
           </View>
-          <View style={[styles.boxes, styles.greenBox]}>
-            <Text style={[styles.boxHeading, styles.boxHeadinggreen]}>
-              Recovered
-            </Text>
-            <Text style={[styles.numbers, styles.boxHeadinggreen]}>
-              2,37,395
-            </Text>
-          </View>
-          <View style={[styles.boxes, styles.grayBox]}>
-            <Text style={[styles.boxHeading, styles.boxHeadinggray]}>
-              Deceased
-            </Text>
-            <Text style={[styles.numbers, styles.boxHeadinggray]}>
-              2,37,395
-            </Text>
-          </View>
-        </View>
-        {/* <View>
+          {/* <View>
           <LineChart
             data={{
               labels: ['January', 'February', 'March', 'April', 'May', 'June'],
@@ -215,7 +291,52 @@ const Dashboard = ({route}) => {
             }
           />
         </View> */}
-      </ScrollView>
+          <View>
+            <LineChart
+              data={{
+                labels: [
+                  'January',
+                  'February',
+                  'March',
+                  'April',
+                  'May',
+                  'June',
+                ],
+                datasets: [
+                  {
+                    data: [Math.random * 100],
+                  },
+                ],
+              }}
+              width={Dimensions.get('window').width} // from react-native
+              height={320}
+              yAxisSuffix="k"
+              yAxisInterval={1} // optional, defaults to 1
+              chartConfig={{
+                backgroundColor: '#fff',
+                backgroundGradientFrom: '#fff',
+                backgroundGradientTo: '#fff',
+                decimalPlaces: 2, // optional, defaults to 2dp
+                color: (opacity = 0) => `rgba(255, 0, 0, ${opacity})`,
+                labelColor: (opacity = 0) => `rgba(0, 0, 0, ${opacity})`,
+                style: {
+                  borderRadius: 16,
+                },
+                propsForDots: {
+                  r: '6',
+                  strokeWidth: '2',
+                  stroke: 'red',
+                },
+              }}
+              bezier
+              style={{
+                marginVertical: 8,
+                borderRadius: 16,
+              }}
+            />
+          </View>
+        </ScrollView>
+      )}
     </View>
   );
 };
